729388c0204dc27f8aabf88747bdf050da869173
Almer June N. Ba-ad <120560008+Meeerr@users.noreply.github.com>
2023-12-08 19:43:12 +0800

    Update main.js

--- main.js
+++ main.js
@@ -35,19 +35,19 @@ function firstQuestion() {
         imageUrl: 'img/logi.gif',
         imageWidth: 300,
         imageHeight: 300,
-        background: '#fff url("img/iput-bg.jpg")',
+        background: '#fff url("iput-bg.jpg")',
         imageAlt: 'Custom image',
         confirmButtonText: CONFIG.btnIntro
     }).then(function() {
         $('.content').show(200);
-        var audio = new Audio('sound/sound.mp3');
+        var audio = new Audio('sound.mp3');
         audio.play();
     })
 }
 
 // switch button position
 function switchButton() {
-    var audio = new Audio('sound/duck.mp3');
+    var audio = new Audio('duck.mp3');
     audio.play();
     var leftNo = $('#no').css("left");
     var topNO = $('#no').css("top");
@@ -60,7 +60,7 @@ function switchButton() {
 }
 // move random button position
 function moveButton() {
-    var audio = new Audio('sound/Swish1.mp3');
+    var audio = new Audio('Swish1.mp3');
     audio.play();
     var x = Math.random() * ($(window).width() - $('#no').width()) * 0.9;
     var y = Math.random() * ($(window).height() - $('#no').height()) * 0.9;
@@ -108,7 +108,7 @@ function textGenerate() {
 
 // show popup
 $('#yes').click(function() {
-    var audio = new Audio('sound/tick.mp3');
+    var audio = new Audio('tick.mp3');
     audio.play();
     Swal.fire({
         title: CONFIG.question,
@@ -116,10 +116,10 @@ $('#yes').click(function() {
         width: 900,
         padding: '3em',
         html: "<input type='text' class='form-control' id='txtReason' onmousemove=textGenerate()  placeholder='Whyyy'>",
-        background: '#fff url("img/iput-bg.jpg")',
+        background: '#fff url("iput-bg.jpg")',
         backdrop: `
               rgba(0,0,123,0.4)
-              url("img/giphy2.gif")
+              url("giphy2.gif")
               left top
               no-repeat
             `,
@@ -131,7 +131,7 @@ $('#yes').click(function() {
             Swal.fire({
                 width: 900,
                 confirmButtonText: CONFIG.btnAccept,
-                background: '#fff url("img/iput-bg.jpg")',
+                background: '#fff url("iput-bg.jpg")',
                 title: CONFIG.mess,
                 text: CONFIG.messDesc,
                 confirmButtonColor: '#83d0c9',
